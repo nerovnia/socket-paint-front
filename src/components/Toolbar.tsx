feb9a25aced481f45987357ba31b73f86a5c1caa
Volodymyr Nerovnia <nerv@i.ua>
2023-12-14 00:47:34 +0200

    Updated Toolbar component

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,15 +1,37 @@
 "use client";
 
 import "../styles/toolbar.scss";
+import toolState from "../store/toolState";
+import canvasState from "../store/canvasState";
+import Brush from "../tools/Brush";
+import Rect from "../tools/Rect";
+import Circle from "../tools/Circle";
+import Eraser from "../tools/Eraser";
+import Line from "../tools/Line";
 
 export default function Toolbar() {
   return (
     <div className="toolbar">
-      <button className="toolbar__btn brush"></button>
-      <button className="toolbar__btn rect"></button>
-      <button className="toolbar__btn circle"></button>
-      <button className="toolbar__btn eraser"></button>
-      <button className="toolbar__btn line"></button>
+      <button
+        className="toolbar__btn brush"
+        onClick={() => toolState.setTool(new Brush(canvasState.canvas))}
+      ></button>
+      <button
+        className="toolbar__btn rect"
+        onClick={() => toolState.setTool(new Rect(canvasState.canvas))}
+      ></button>
+      <button
+        className="toolbar__btn circle"
+        onClick={() => toolState.setTool(new Circle(canvasState.canvas))}
+      ></button>
+      <button
+        className="toolbar__btn eraser"
+        onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}
+      ></button>
+      <button
+        className="toolbar__btn line"
+        onClick={() => toolState.setTool(new Line(canvasState.canvas))}
+      ></button>
       <input style={{ marginLeft: 10 }} type="color"></input>
       <button className="toolbar__btn undo"></button>
       <button className="toolbar__btn redo"></button>
